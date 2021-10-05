60e9678b6ee877f7953a89b55bf2a29fa9a3c43c
Disha <dishapatil2002@gmail.com>
2021-10-05 22:18:22 +0530

    done

--- Assignments/Assignment6.py
+++ Assignments/Assignment6.py
@@ -3,4 +3,3 @@ Write a python program to store first year percentage of students in array.
 Write function for sorting array of floating point numbers in ascending order using quick sort and display top five scores.
 '''
 
-# Done by SCOC12 Disha Patil
