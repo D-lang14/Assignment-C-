93dd9674d0b7171f26fa45d72aacc260bfe8c487
Disha <dishapatil2002@gmail.com>
2021-10-05 22:20:41 +0530

    done

--- Assignments/Assignment13.cpp
+++ Assignments/Assignment13.cpp
@@ -4,5 +4,3 @@ Orders are served in first come first served basis.
 Order once placed cannot be cancelled. 
 Write C++ program to simulate the system using circular queue using array. 
 */
-
-// Done by SCOC12 Disha Patil
