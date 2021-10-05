ac8989601149a3d7792ca640d1ad6a8fcc7b5a84
Disha <dishapatil2002@gmail.com>
2021-10-05 22:20:21 +0530

    done

--- Assignments/Assignment12.cpp
+++ Assignments/Assignment12.cpp
@@ -4,5 +4,3 @@ deletions may be made at either end.
 Obtain a data representation mapping a deque into a one-dimensional array. 
 Write C++ program to simulate deque with functions to add and delete elements from either end of the deque
 */
-
-// Done by SCOC12 Disha Patil
