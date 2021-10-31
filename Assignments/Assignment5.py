d8f759ea780b7cca35666c708d22a4f776f8d0e7
Disha <dishapatil2002@gmail.com>
2021-10-31 08:34:59 +0530

    Modified 5

--- Assignments/Assignment5.py
+++ Assignments/Assignment5.py
@@ -4,35 +4,38 @@ Write function for sorting array of floating point numbers in ascending order us
 a) Selection Sort 
 b) Bubble sort and display top five scores.
 '''
-
-import array as arr
-
-a = arr.array('f', [])
-
-k = int(input("Enter the number of Students: "))
-for i in range(0, k):
-    a.append(float(input("Enter the First Year % of Student[{0}] : ".format(i))))
+# Example
+# a = [5,3,1,9,8,2,4,7]
 
 
-    
-    
-# BUBBLE SORT
+# SELECTION SORT of elements
+
+def selectionSort(arr):
+    for i in range(len(arr)):
+        minIdx = i 
+        for j in range(i+1, len(arr)):
+            if(arr[minIdx] > arr[j]):
+                minIdx = j
+        arr[minIdx],arr[i] = arr[i], arr[minIdx]
+
+    print("Marks of students after performing Selection Sort on the list : ")
+    for i in range(len(marks)):
+        print(marks[i])
+
+
+# BUBBLE SORT of elements
 
 def bubbleSort(arr):
     n = len(arr)
     for i in range(n-1):
+        # Last i elements are already in place
         for j in range(0, n-i-1):
             if(arr[j]>arr[j+1]):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
-bubbleSort(a)   #Function calling
-
-print("\nSorted Marks List using Bubble Sort")
-for i in range(len(a)):
-    print(a[i], end=" ")
-
-
-    
+    print("Marks of students after performing Bubble Sort on the list :")
+    for i in range(len(marks)):
+        print(marks[i])
 
 
 # IMPROVED VERSION OF BUBBLE SORT
@@ -47,31 +50,86 @@ def improvedBubbleSort(arr):
             if(arr[j]>arr[j+1]):
                 flag = 1
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-
-improvedBubbleSort(a) 
-
-print("\nSorted Marks List using Improved Version of Bubble Sort")
-for i in range(len(a)):
-    print(a[i], end=" ")
+                
+    print("Marks of students after performing Improved Bubble Sort on the list :")
+    for i in range(len(marks)):
+        print(marks[i])
 
 
-    
+# Displaying top five marks
 
-# SELECTION SORT
-
-def selectionSort(arr):
-    for i in range(len(arr)):
-        k = i 
-        for j in range(i+1, len(arr)):
-            if(arr[k] > arr[j]):
-                k = j
-        arr[k],arr[i] = arr[i], arr[k]
-
-selectionSort(a)
-
-print("\nSorted Marks List using Selection Sort")
-for i in range(len(a)):
-    print(a[i], end=" ")
+def topFiveMarks(marks):
+    print("Top",len(marks),"Marks are : ")
+    print(*marks[::-1],"\n")
 
 
-# a = [5,3,1,9,8,2,4,7]
+# Main
+
+marks=[]
+n = int(input("Enter number of students whose marks are to be displayed : "))
+print("Enter marks for",n,"students (Press ENTER after every students marks): ")
+for i in range(0, n):
+    element = int(input())
+    marks.append(element) 
+
+print("The marks of",n,"students are : ")
+print(marks)
+
+flag=1;
+while flag==1:
+    print("\n---------------MENU---------------")
+    print('''
+1. Selection Sort of the marks
+2. Bubble Sort of the marks
+3. Improved Bubble Sort of the marks
+4. Exit
+    ''')
+
+    ch=int(input("\n\nEnter your choice (from 1 to 4) : "))
+
+    if ch==1:
+        selectionSort(marks)
+        a=input("\nDo you want to display top marks from the list (yes/no) : ")
+        if a=='yes':
+            topFiveMarks(marks)
+        else:
+            print("\nDone!")
+            flag=0
+
+    elif ch==2:
+        bubbleSort(marks)
+        a = input("\nDo you want to display top five marks from the list (yes/no) : ")
+        if a == 'yes':
+            topFiveMarks(marks)
+        else:
+            print("\nDone!")
+            flag = 0
+
+    elif ch == 3:
+        improvedBubbleSort(marks)
+        a = input("\nDo you want to display top five marks from the list (yes/no) : ")
+        if a == 'yes':
+            topFiveMarks(marks)
+        else:
+            print("\nDone!")
+            flag = 0
+
+    elif ch == 4:
+        a=input("\nDo you want to continue (yes/no): ")
+        if a=="yes":
+            flag=1
+        else:
+            flag=0
+            print("Done!")
+
+    else:
+        print("\nEnter a valid choice!!")
+        a=input("\nDo you want to continue (yes/no): ")
+        if a=="yes":
+            flag=1
+        else:
+            flag=0
+            print("Done!")
+
+#<----------------------------------------END OF PROGRAM---------------------------------------->
+
