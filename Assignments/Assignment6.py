262ff21405757c3d69d938474c914f79b8bc73d8
Disha <dishapatil2002@gmail.com>
2021-10-31 08:35:50 +0530

    Modified 6

--- Assignments/Assignment6.py
+++ Assignments/Assignment6.py
@@ -3,14 +3,25 @@ Write a python program to store first year percentage of students in array.
 Write function for sorting array of floating point numbers in ascending order using quick sort and display top five scores.
 '''
 
-import array as arr
 
-a = arr.array('f', [])
+# Accepting the percentage of the Students
 
-k = int(input("Enter the number of Students: "))
-for i in range(0, k):
-    a.append(float(input("Enter the First Year % of Student[{0}] : ".format(i))))
+def acceptPercentage():
+    percentage = [];
+    numberOfStudents = int(input("Enter the number of Student: "))
+    for i in range(numberOfStudents):
+        percentage.append(float(input("Enter the Percentage(float) of Students[{0}]: ".format(i + 1))))
+    return percentage
 
+
+# Displaying the percentage of the Students
+
+def displayPercentage(percentage):
+    for i in range(len(percentage)):
+        print(percentage[i],end=" ")
+
+
+# Performing partition of the Data
 
 def partition(left, right, arr):
     index = left
@@ -29,18 +40,77 @@ def partition(left, right, arr):
     return right
 
 
-def quickSort(left, right, arr):
-    if(left < right):
+# Performing Quick Sort on the Data
+
+def quickSort(arr, left, right):
+    while left < right :
         p = partition(left, right, arr)
+        quickSort(arr, left, p-1)
+        quickSort(arr, p+1, right)
+        return arr
+    
+# Displaying Top Five Percentages of Students
 
-        quickSort(left, p-1, arr)
-        quickSort(p+1, right, arr)
+def displayTopFive(percentage):
+    print("Top Five Percentages are : ")
+    if len(percentage) < 5:
+        start, stop = len(percentage) - 1, -1
+    else:
+        start, stop = len(percentage) - 1, len(percentage) - 6
+
+    for i in range(start, stop, -1):
+        print(percentage[i],"\n")
 
 
-quickSort(0, len(a)-1, a)
+# Main 
+
+unsortedPercentage = []
+sortedPercentage = []
+flag = 1
+
+while flag == 1:
+    print("\n--------------------MENU--------------------")
+    print('''
+1. Accept the Percentage of Students
+2. Display the Percentages of Students
+3. Perform Quick Sort on the Data
+4. Exit
+    ''')
+
+    ch = int(input("Enter your choice (from 1 to 4) : "))
+
+    if ch == 1:
+        unsortedPercentage = acceptPercentage()
+
+    elif ch == 2:
+        displayPercentage(unsortedPercentage)
+
+    elif ch == 3:
+        print("Percentages of Students after performing Quick Sort : ")
+        sortedPercentage = quickSort(unsortedPercentage,0,len(unsortedPercentage)-1)
+        displayPercentage(sortedPercentage)
+
+        a = input("Do you want to display the Top 5 Percentages of Students (yes/no) : ")
+        if a == 'yes':
+            displayTopFive(sortedPercentage)
+
+    elif ch == 4:
+        a=input("\n\nDo you want to continue (yes/no): ")
+        if a=="yes":
+            flag=1
+        else:
+            flag=0
+            print("Done!")
+
+    else:
+        print("Invalid Choice!!")
+        a=input("\n\nDo you want to continue (yes/no): ")
+        if a=="yes":
+            flag=1
+        else:
+            flag=0
+            print("Done!")
 
 
-print("\nSorted Marks List")
-for i in range(0, len(a)):
-    print("{0:.2f}\t".format(a[i]), end=" ")
-print()
+#<------------------------------------END OF PROGRAM------------------------------------>
+
